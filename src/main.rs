3ed3b48535049d6bc1b5cee121d5171559d93699
Crawl <deathninja.paul@gmail.com>
2025-12-03 09:32:10 +0800

    Finally done

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,38 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("l/input2.txt").unwrap();
+    let input = fs::read_to_string("l/input.txt").unwrap();
     let lines = input.lines();
-    let mut previous_pos: i32 = 0;
     let mut pos = 50;
-    let mut total_hits = 0i32;
-    // let mut password = 0;
+    let mut password = 0;
     for line in lines {
         let direction = line.bytes().next().unwrap();
         let steps = line[1..].parse::<i32>().unwrap();
-        // previous_pos = pos;
         if direction == b'L' {
-            let asa = pos - steps;
-            let a = asa.div_euclid(100).abs();
-            if (a != 0 || pos == steps) && pos != 0 {
-                total_hits += 1;
+            let displacement = pos - steps;
+            let mut hits = displacement.div_euclid(100).abs();
+            if displacement == 0 {
+                if pos != 0 {
+                    password += 1;
+                }
+            } else if displacement < 0 {
+                if pos == 0 {
+                    hits -= 1;
+                }
+                if displacement % 100 == 0 {
+                    password += 1;
+                }
+                password += hits;
+            } else {
+                password += hits;
             }
-            pos = asa.rem_euclid(100);
+            pos = displacement.rem_euclid(100);
         } else {
-            let asa = pos + steps;
-            let a = asa.div_euclid(100).abs();
-            if (a != 0 || pos == steps) && pos != 0 {
-                total_hits += 1;
-            }
-            pos = asa.rem_euclid(100);
+            let displacement = pos + steps;
+            let hits = displacement.div_euclid(100).abs();
+            password += hits;
+            pos = displacement.rem_euclid(100);
         }
-        // expect 1 1 2 2 3 4 4 5 6
-        print!("{} ", total_hits);
+        println!("{}", password);
     }
-    println!("");
-    // println!("total rotations: {}", total_hits);
 }
